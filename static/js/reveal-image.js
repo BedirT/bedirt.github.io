58e1a9da9c0ce631ece8b2fe51cfa7a58ca4e911
Bedir Tapkan <tapkan@ualberta.ca>
2023-03-30 05:56:31 -0600

    touch functionality fixed on hoverimage

--- static/js/reveal-image.js
+++ static/js/reveal-image.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hoverImageContainers = document.querySelectorAll('.hover-image-container');
+    const lerp_factor_desktop = 0.0004;
+    const lerp_factor_mobile = 0.2;
 
     function lerp(start, end, t) {
         // Linear interpolation
         return start * (1 - t) + end * t;
     }
   
-    function animateClipPath(container, targetPercentage) {
+    function animateClipPath(container, targetPercentage, lerp_factor) {
         const currentPercentage = parseFloat(container.getAttribute('data-current-percentage') || '100');
-        const newPercentage = lerp(currentPercentage, targetPercentage, 0.0004);
+        const newPercentage = lerp(currentPercentage, targetPercentage, lerp_factor);
 
         container.querySelector('.textured-image').style.clipPath = `polygon(0% 0, calc(${newPercentage}% + 1px) 0, calc(${newPercentage}% + 1px) 100%, 0% 100%)`;
         container.setAttribute('data-current-percentage', newPercentage);
@@ -23,34 +25,56 @@ document.addEventListener('DOMContentLoaded', function () {
         container.setAttribute('data-target-percentage', percentage);
     }
 
-    function hoverAnimation(container) {
+    function hoverAnimation(container, lerp_factor) {
         const targetPercentage = parseFloat(container.getAttribute('data-target-percentage') || '100');
-        animateClipPath(container, targetPercentage);
-        const shouldContinue = animateClipPath(container, targetPercentage);
+        const shouldContinue = animateClipPath(container, targetPercentage, lerp_factor);
         if (shouldContinue) {
-            requestAnimationFrame(() => hoverAnimation(container));
+            requestAnimationFrame(() => hoverAnimation(container, lerp_factor));
         }
         console.log('hoverAnimation');
     }
 
     hoverImageContainers.forEach(function (container) {
+        // Mouse event handlers
         function startHover(event) {
             container.setAttribute('data-mouse-over', 'true');
             updateTargetPercentage(container, event);
-            hoverAnimation(container);
+            hoverAnimation(container, lerp_factor_desktop);
         }
 
         function updateHover(event) {
             updateTargetPercentage(container, event);
             if (container.getAttribute('data-mouse-over') === 'true') {
-                hoverAnimation(container);
+                hoverAnimation(container, lerp_factor_desktop);
             }
         }
 
         function endHover() {
             if (container.getAttribute('data-mouse-over') === 'true') {
                 container.setAttribute('data-target-percentage', '100');
-                hoverAnimation(container);
+                hoverAnimation(container, lerp_factor_desktop);
+            }
+            container.setAttribute('data-mouse-over', 'false');
+        }
+
+        // Touch event handlers
+        function startTouch(event) {
+            event.preventDefault();
+            container.setAttribute('data-mouse-over', 'true');
+            updateTargetPercentage(container, event.touches[0]);
+            hoverAnimation(container, lerp_factor_mobile);
+        }
+
+        function updateTouch(event) {
+            event.preventDefault();
+            updateTargetPercentage(container, event.touches[0]);
+            hoverAnimation(container, lerp_factor_mobile);
+        }
+
+        function endTouch() {
+            if (container.getAttribute('data-mouse-over') === 'true') {
+                container.setAttribute('data-target-percentage', '100');
+                hoverAnimation(container, lerp_factor_mobile);
             }
             container.setAttribute('data-mouse-over', 'false');
         }
@@ -61,10 +85,10 @@ document.addEventListener('DOMContentLoaded', function () {
         container.addEventListener('mouseleave', endHover);
 
         // Touch event listeners
-        container.addEventListener('touchstart', startHover);
-        container.addEventListener('touchmove', updateHover);
-        container.addEventListener('touchend', endHover);
-        container.addEventListener('touchcancel', endHover);
+        container.addEventListener('touchstart', startTouch);
+        container.addEventListener('touchmove', updateTouch);
+        container.addEventListener('touchend', endTouch);
+        container.addEventListener('touchcancel', endTouch);
     });
 });
   
